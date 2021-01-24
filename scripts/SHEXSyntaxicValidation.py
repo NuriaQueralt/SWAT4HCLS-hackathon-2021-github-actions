578aaee92c65da7aeb7a8b26802ec9ec7cd49f10
NuriaQueralt <nqueralt.r@gmail.com>
2021-01-24 19:02:46 +0100

    SHEXSyntaxicValidation.py: added the validation of the shex schema oject

--- scripts/SHEXSyntaxicValidation.py
+++ scripts/SHEXSyntaxicValidation.py
@@ -2,6 +2,8 @@ import glob
 import logging
 #from rdflib import Graph
 import sys
+from pyshex.utils.schema_loader import SchemaLoader
+from io import StringIO
 
 # set log level
 logging.basicConfig(level=logging.INFO)
@@ -15,11 +17,20 @@ for extension in shex_file_extension.keys() :
     for filename in glob.iglob(root_path + files_to_check, recursive=True):
          logging.info("Validating file " + filename)
          try:
-             
-             
+             shex = open(filename,"r")
+             log = StringIO()
+             if isinstance(shex, str):
+                 shex = SchemaLoader().loads(shex)
+             else:
+                 shex = SchemaLoader().load(shex)
+                 shex['@context'] = "http://www.w3.org/ns/shex.jsonld"
          except Exception as e:
              logging.error(e)
-             logging.error("Syntaxic error reading ShEx file [" +filename+"]")
+             logging.error("Syntactic error reading ShEx file [" +filename+"]")
              sys.exit(1)
+    else:
+        print(f"The ShEx schema object is valid: {shex._is_valid()}")
+        if not shex._is_valid(log):
+            print(log.getvalue())
 
-print("ShEx files syntaxic validation is successful")
+print("ShEx files syntactic validation is successful")
